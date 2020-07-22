e557c34ffb57793ee173d3bb1d2e7314f6024bbd
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2020-07-22 11:20:17 +0300

    feat: add login and register pages

--- src/navigation.js
+++ src/navigation.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Publications from "./pages/publications";
 import ShareToughtsPage from "./pages/share-thoughts";
+import RegisterPage from "./pages/register";
+import LoginPage from "./pages/login";
 
 const Navigation = () => {
   return (
@@ -10,6 +12,8 @@ const Navigation = () => {
       <Switch>
           <Route path="/" exact component={Publications} />
           <Route path="/share" component={ShareToughtsPage} />
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/login" component={LoginPage} />
       </Switch>
     </BrowserRouter>
   );
