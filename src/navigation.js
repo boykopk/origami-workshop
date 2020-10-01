22a1118e8abc22965ab087aebf4e46087e41a7b0
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2020-10-01 14:18:19 +0300

    fix: add error boundary

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import Publications from "./pages/publications";
 import ShareToughtsPage from "./pages/share-thoughts";
@@ -7,16 +7,28 @@ import RegisterPage from "./pages/register";
 import LoginPage from "./pages/login";
 import ProfilePage from "./pages/profile";
 import ErrorPage from "./pages/error";
+import UserContext from './Context';
 
 const Navigation = () => {
+  const context = useContext(UserContext);
+  const loggedIn = context.user.loggedIn;
+
   return (
     <BrowserRouter>
       <Switch>
           <Route path="/" exact component={Publications} />
-          <Route path="/share" component={ShareToughtsPage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/profile/:userid" component={ProfilePage} />
+          <Route path="/share">
+            {loggedIn ? (<ShareToughtsPage />): (<Redirect to="/login" />)}
+          </Route>
+          <Route path="/register">
+            {loggedIn ? (<Redirect to="/" />) : (<RegisterPage />)}
+          </Route>
+          <Route path="/login">
+            {loggedIn ? (<Redirect to="/" />) : (<LoginPage />)}
+          </Route>
+          <Route path="/profile/:userid">
+            {loggedIn ? (<ProfilePage />): (<Redirect to="/login" />)}
+          </Route>
           <Route component={ErrorPage} />
       </Switch>
     </BrowserRouter>
